3c040d82b825805d2b9277102ad419bcc500c966
Richard Gunning <rjgunning@gmail.com>
2015-08-04 15:16:29 +0100

    style fix

--- lib/App/Pipeline/Lite4/Command/run.pm
+++ lib/App/Pipeline/Lite4/Command/run.pm
@@ -26,8 +26,8 @@ sub execute {
         $App_Pipeline_Lite->util->append_description( $run_info, $desc ); # Write Options to log
         
         $App_Pipeline_Lite->smoke_test(1) if defined($opt->{smoke_test});
-        defined $opt->{steps} ? $App_Pipeline_Lite->step_filter_str($step_filter);
-        defined $opt->{jobs} ? $App_Pipeline_Lite->job_filter_str( $job_filter );
+        $App_Pipeline_Lite->step_filter_str($step_filter) if defined $opt->{steps};
+        $App_Pipeline_Lite->job_filter_str( $job_filter ) if defined $opt->{jobs};
         
         $App_Pipeline_Lite->run_num($run_num);
         
